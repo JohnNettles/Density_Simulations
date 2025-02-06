0f0be723d8dc6c1d41ec253b075ff51eff34097c
JohnNettles <nettle2@g.clemson.edu>
2025-02-06 14:53:59 -0500

    Completed the REM script. Works with varying EDD and EDA. Includes bootstraps

--- Random.Encounter.R
+++ Random.Encounter.R
@@ -10,8 +10,8 @@ Dens <- (y/t) * (pi/(v*r*(2+theta)))
 
 # Define variables --------------------------------------------------------
 
-r <- r
-theta <- theta
+r.EDD <- r.EDD
+theta.EDA <- theta.EDA
 v <- 0.5 #converted to meters per minute
 t <- n_steps
 
@@ -33,45 +33,53 @@ y <- counts
 
 # Calculate density for each camera ---------------------------------------
 #answer is animals per square meter
+#multiply by area of study space (in square meters) to get abundance
 
-density <- rep(NA, n_cams)
-
-for(i in 1:n_cams) {
+REM <- function(y,t,v,r,theta,n_cams) {
   
-  density[i] <- (y[i]/t) * (pi/(v*r*(2+theta)))
-}
-
-abundance <- mean(density) * 1000000 * (sa.width/1000)*(sa.length/1000)
-
-# Bootstrap for variance
-
-for (i in 1:10000) {
+  dens <- rep(NA,n_cams)
   
-  bootstrap <- sample(density,length(density),replace=T)
-  bs.sd[i] <- sd(bootstrap)
-}
-
-
-
-
-P <- A/a # Number of possible cameras (grid cells) in study area
-
-boot.fn <- function(data, nboot, P) {
-  estN <- rep(NA, nboot)
-  for (i in 1:nboot){
-    boot.dat <- list(count = matrix(sample(data$count, data$ncam*data$nocc, replace = T), 
-                                    nrow = data$ncam),
-                     ncam = data$ncam,
-                     nocc = data$nocc) 
-    estN[i] <- IS.fn(boot.dat, P)
+  for(i in 1:n_cams) {
+    dens[i] <- (y[i]/t) * (pi/(v*r.EDD[i]*(2+theta.EDA[i])))
   }
-  return(estN)
+  
+  return(dens)
 }
 
-estN.boot <- boot.fn(data.list, nboot = 100, P = P)
+REM.res <- REM(y=y,t=t,v=v,r=r.EDD,theta=theta.EDA,n_cams=n_cams)
 
-se_estN <- sd(estN.boot)
+abundance <- mean(REM.res) * A 
+
+
+
+# Bootstrap for variance --------------------------------------------------
+
+boot.fn <- function(data,nboot) {
+  counts <- rep(NA,n_cams)
+  boot.rem <- rep(NA,nboot)
+  
+  for (i in 1:nboot) {
+    #Resample detections
+    boot.caps <- matrix(sample(data,n_cams*n_steps,replace=T),nrow=n_cams) 
+    
+    #need to sum the detections for each row (camera)
+    counts <- rowSums(boot.caps)
+    
+    #Calculate the average density for each bootstrap iteration
+    boot.rem[i] <- mean(REM(y = counts, t=t, v=v, r=r.EDD,theta=theta.EDA, n_cams=n_cams)) 
+  }
+  
+  return(boot.rem)
+}
+  
+#Run the function
+estD.boot <- boot.fn(captures, nboot = 100)
+
+#Convert to abundance
+meanN_boot <- mean(estD.boot)*A
+meanN_boot
+se_estN <- sd(estD.boot)*A
 se_estN
-CI_estN <- quantile(estN.boot, c(0.025, 0.975))
+CI_estN <- quantile(estD.boot*A, c(0.025, 0.975))
 CI_estN
 
