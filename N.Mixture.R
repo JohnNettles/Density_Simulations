610f853981769a24a84b7a613888fc92f2de71f2
JohnNettles <nettle2@g.clemson.edu>
2025-03-13 14:02:51 -0400

    Added two models using the spAbundance package

--- N.Mixture.R
+++ N.Mixture.R
@@ -4,8 +4,6 @@ library(dplyr)
 library(lubridate)
 library(tidyr)
 
-
-
 # Create timestamps and format data ---------------------------------------
 
 #Create made up start time and add 1 minute for each simulated step
@@ -95,10 +93,23 @@ counts <- counts[,-1]
 
 #Convert to matrix
 detection_matrix <- as.matrix(counts)
+
+#Create a full list of camera names
+all_cameras <- rep(NA,n_cams)
+for (i in 1:n_cams){
+  name <- paste("Camera",i,sep="_")
+  all_cameras[i] <- name
+}
+
+#Create matrix of zeros for cameras with no detections
+n_bins = dim(detection_matrix)[2]
+cam_nodetect <- all_cameras[!all_cameras %in% rownames(counts)]
+blanks <-array(0, dim=c(length(cam_nodetect),ncols=n_bins))
+row.names(blanks) <- c(cam_nodetect)
+
+#Combine detections matrix and blanks
+detection_matrix <- rbind(detection_matrix,blanks)
 dm <- c(detection_matrix)
-
-
-
 
 
 
@@ -110,109 +121,32 @@ n_bins = dim(detection_matrix)[2]
 
 # also need a site index and covariates that have the same length 
 # as the variable C
-site = 1:R                # ‘Short’ version of site covariate; list of sites
-site.p <- rep(site, n_bins)    # ‘Long’ version of site covariate; repeat for each rep
+cam = 1:R                # ‘Short’ version of site covariate; list of sites
+camera.p <- rep(site, n_bins)    # ‘Long’ version of site covariate; repeat for each rep
 cbind(dm, site.p)  # Check that all went right
 
-jags.dat <- list(R = R, n = n, C = dm, site.p = site.p)
+movement_radius <- max_speed/1000
 
-
-
-# Poisson process ---------------------------------------------------------
-
-##### SPECIFY MODEL CODE #####
-cat(file="model.txt", "
-model {
-
-    # Priors
-    alpha.lam ~ dunif(-10,10)
-    alpha.p ~ dunif(-10,10)
-    
-    # Likelihood
-    # Biological model for true abundance
-    # Loop over R sites
-    for (i in 1:R) {                        
-      N[i] ~ dpois(lambda[i])
-      Dens[i] <- N[i]/(2.62*2.62*3.14159)
-      log(lambda[i]) <- alpha.lam
-    }
-    
-    # Observation model for replicated counts
-    # Loop over all n observations; i is 1:112 here
-    for (i in 1:n){
-      C[i] ~ dbin(p[i],N[site.p[i]]) #site.p is just 1 to R for each rep (length of 112)
-      lp[i] <- alpha.p  #logit(p) follows linear relationship
-      p[i] <- exp(lp[i])/(1+exp(lp[i])) #logit link
-    }
-    
-    # Derived quantities
-    # Estimate total population size across all sites
-    totalN <- sum(N[]) #sum of all site-specific abundances
-    AvgDens <- mean(Dens[])
-    
-    # Assess model fit using Chisquare discrepancy
-    for (i in 1:n) {
-    
-      #Compute fit statistics for observed data
-      eval[i] <- p[i]*N[site.p[i]]
-      E[i] <- pow((C[i]-eval[i]),2)/(eval[i] + 0.5) #Error
-      
-      #Generate replicate data and compute fit statistics
-      C.new[i] ~ dbin(p[i],N[site.p[i]])
-      E.new[i] <- pow((C.new[i]-eval[i]),2)/(eval[i] + 0.5)
-    }
-    
-    fit <- sum(E[])
-    fit.new <- sum(E.new[])
-    
-    
-}
- ")
-
-##### INITIAL VALUES #####
-# Inits function
-# use the maximum count at each site as a first guess of what N might be 
-# add 1 to avoid zeros
-Nst <- apply(detection_matrix, 1, max) + 1 #max count of observed for each site plus 1 (so no zeros to avoid JAGS issues)
-inits <- function(){list(N = Nst, alpha.lam=rnorm(1, 0, 1), 
-                         alpha.p=rnorm(1, 0, 1))}
-
-##### PARAMETERS TO MONITOR #####
-params <- c("N", "totalN", "Dens", "AvgDens", "alpha.lam", "alpha.p",
-            "fit","fit.new")
-
-##### MCMC DIMENSIONS #####
-nc = 3
-nb =20000
-ni = 50000
-nt =50
-n.iter = ni + nb
-
-##### RUN THE MODEL IN JAGS #####
-starttime = Sys.time()
-jags.fit =  jags(data = jags.dat, inits = inits, parameters.to.save = params, model.file = "model.txt", 
-                 n.thin = nt, n.chains = nc, n.burnin = nb, n.iter = n.iter, parallel = F)
-Sys.time() - starttime
-
+jags.dat <- list(R = R, n = n, C = dm, camera.p = camera.p, movement_radius=movement_radius)
 
 
 # Negative Binomial -------------------------------------------------------
 
 ##### SPECIFY MODEL CODE #####
-cat(file="model.text","
+cat(file="model.txt","
 model {
 
     #Priors
     alpha.lam ~ dunif(-10,10)
-
     alpha.p ~ dunif(-10,10)
-
+    r ~ dunif(0,50)
+    
     #Likelihood
     # Biological model for true abundance
     # Loop over R sites
         for (i in 1:R) {
           N[i] ~ dnegbin(p.nb[i],r)
-          Dens[i] <- N[i]/(2.62*2.62*3.14159)
+          Dens[i] <- N[i]/(movement_radius*movement_radius*3.14159)
           p.nb[i] <- r/(r+lambda[i])
           log(lambda[i]) <- alpha.lam
         }
@@ -220,8 +154,8 @@ model {
     # Observation model for replicated counts
     # Loop over all n observations; i is 1:600 here
         for (j in 1:n){
-          C[j] ~ dbin(p[j],N[site.p[j]])
-          lp[j] <- alpha.p  #logit(p) follows linear relationship
+          C[j] ~ dbin(p[j],N[camera.p[j]])
+          lp[j] <- alpha.p #logit(p) follows linear relationship
           p[j] <- exp(lp[j])/(1+exp(lp[j])) #logit link
      }
         
@@ -234,11 +168,11 @@ model {
     for (i in 1:n) {
     
       #Compute fit statistics for observed data
-      eval[i] <- p[i]*N[site.p[i]]
+      eval[i] <- p[i]*N[camera.p[i]]
       E[i] <- pow((C[i]-eval[i]),2)/(eval[i] + 0.5) #Error
       
       #Generate replicate data and compute fit statistics
-      C.new[i] ~ dbin(p[i],N[site.p[i]])
+      C.new[i] ~ dbin(p[i],N[camera.p[i]])
       E.new[i] <- pow((C.new[i]-eval[i]),2)/(eval[i] + 0.5)
     }
     
@@ -262,8 +196,8 @@ params <- c("N","Dens", "totalN","AvgDens", "alpha.lam", "alpha.p","fit","fit.ne
 
 ##### MCMC DIMENSIONS #####
 nc = 3
-nb =3000
-ni = 6000
+nb =20000
+ni = 30000
 nt =50
 n.iter = ni + nb
 
@@ -297,3 +231,101 @@ mean(jags.fit$sims.list$fit.new > jags.fit$sims.list$fit) #for each iteration, i
 
 # check betas
 whiskerplot(jags.fit, parameters="N", quantiles=c(0.025,0.975), zeroline=TRUE)
+
+
+
+# spAbundance -------------------------------------------------------------
+library(spAbundance)
+abund_formula <- ~1
+det_formula <- ~1
+
+#Priors
+priors.list <- list(beta.normal = list(mean = 0, 
+                                      var = 50),
+                   alpha.normal = list(mean = 0,
+                                       var = 3), 
+                   kappa.unif = c(0, 10)) 
+# Starting values
+inits.list <- function() {
+  list(
+    alpha = rnorm(1, 0, 1),
+    beta = rnorm(1, 0, 1),
+    kappa = runif(1, 1, 10),
+    N = apply(detection_matrix, 1, max, na.rm=TRUE) + 1
+  )
+}
+  
+  
+N.fit <- NMix(abund_formula, det_formula,
+            data = list(y = detection_matrix),
+            priors = priors.list,
+            inits = inits.list(),
+            family = 'NB',
+            n.batch = 2000,
+            batch.length = 25,
+            n.burn = 5000,
+            n.thin = 50,
+            n.chains = 3,
+            verbose = TRUE)
+
+summary(N.fit)
+
+#Posterior predictive check
+ppc <- ppcAbund(fit,fit.stat='chi-squared',group=1, type='conditional')
+summary(ppc)
+
+########## Spatial N Mixture ###############
+
+coords_matrix <- data.frame(cameras$X,cameras$Y)
+row.names(coords_matrix) <- c(all_cameras)
+coords_matrix <- coords_matrix[order(row.names(coords_matrix)),]
+
+
+coords_sp <- SpatialPointsDataFrame(coords_matrix, as.data.frame(all_cameras))
+row.names(coords_sp) <- c(all_cameras)
+coords_sp <- coords_sp[order(row.names(coords_sp)),]
+
+detection_matrix <- detection_matrix[order(rownames(detection_matrix)),]
+sp_detect <- SpatialPointsDataFrame(coords_sp, as.data.frame(detection_matrix))
+
+abund_formula <- ~1
+det_formula <- ~1
+
+spN.data <- list(y=detection_matrix, coords=coords_matrix)
+
+#Priors
+prior.list <- list(beta.normal = list(mean = 0, 
+                                      var = 20),
+                   alpha.normal = list(mean = 0,
+                                       var = 3), 
+                   kappa.unif = c(0, 10))
+# Starting values
+inits.list <- function() {
+  list(
+    alpha = rnorm(1, 0, 1),
+    beta = rnorm(1, 0, 1),
+    kappa = runif(1, 1, 10),
+    N = apply(detection_matrix, 1, max, na.rm=TRUE) + 1
+  )
+}
+
+spN.fit <- spNMix(abund_formula, det_formula,
+              data = spN.data,
+              inits = inits.list(),
+              priors = priors.list,
+              cov.model = 'exponential',
+              n.neighbors = 15,
+              search.type = 'cb',
+              family = 'NB',
+              n.batch = 2000,
+              batch.length = 25,
+              n.burn = 5000,
+              n.thin = 50,
+              n.chains = 3,
+              verbose = TRUE)
+
+summary(fit)
+
+#Posterior predictive check
+ppc <- ppcAbund(fit,fit.stat='chi-squared',group=1, type='conditional')
+summary(ppc)
